d4fa3d26b6c68dd2e1cb6313a26bb80ac43304ff
alan <french.alan97@gmail.com>
2018-07-04 17:15:47 -0400

    added a profile page

--- pals/urls.py
+++ pals/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.pals_list, name='pals_list'),
+    url(r'^pal/(?P<name>\w+)/$', views.pal_profile, name='pal_profile'),
 ]
--- pals/views.py
+++ pals/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Pal
 # Create your views here.
 
 def pals_list(request):
     pals = Pal.objects.all()
     return render(request,'pals/pals_list.html',{'pals':pals})
+
+def pal_profile(request,name):
+    pal = get_object_or_404(Pal,name=name)
+    return render(request, 'pals/pal_profile.html', {'pal':pal})
